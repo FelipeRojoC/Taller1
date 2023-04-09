e029d37b8a0eba0433401ebe18f86c0ba84f42f2
FelipeRojoC <felipe.rojo@alumnos.ucn.cl>
2023-04-08 21:17:32 -0400

    3 Commit (Ultimo)

--- src/Main.java
+++ src/Main.java
@@ -16,23 +16,26 @@ public class Main {
 
         //Buffering
         StdDraw.enableDoubleBuffering();
-        crearLinea(minimo,maximo);
+
+        //Generar un numero random entre 0 y 0.05 para la velocidad
+        double minimoVelocidad = 0;
+        double maximoVelocidad = 0.05;
+        double randomVelocidad = minimoVelocidad + (maximoVelocidad-minimoVelocidad) * Math.random();
+
+        //Crear lineas
+        crearLinea(minimo,maximo,randomVelocidad);
     }
 
-    private static double crearLinea(double minimo, double maximo) {
+    private static double crearLinea(double minimo, double maximo, double randomVelocidad) {
 
-        /**
-         *Generar velocidad al azar
-         *la velocidad al azar va muy rapido por eso lo configure
-         *para que fuera mas lento pero el codigo lo dejo comentado
-         */
-        double x0Velocidad = 0.005;//minimo + (maximo-minimo) * Math.random();
-        double y0Velocidad = 0.005;//minimo + (maximo-minimo) * Math.random();
-        double x1Velocidad = 0.005;//minimo + (maximo-minimo) * Math.random();
-        double y1Velocidad = 0.005;//minimo + (maximo-minimo) * Math.random();
+        //Asignar velocidad de las lineas
+        double x0Velocidad = randomVelocidad;
+        double y0Velocidad = randomVelocidad;
+        double x1Velocidad = randomVelocidad;
+        double y1Velocidad = randomVelocidad;
 
         //Definir seis colores
-        Color[] colores = {Color.BLACK, Color.RED, Color.BLUE, Color.ORANGE, Color.GREEN, Color.PINK};
+        Color[] colores = {Color.BLACK, Color.RED, Color.BLUE, Color.ORANGE, Color.GREEN, Color.CYAN};
 
         //Definir una linea al azar
         double x0 = minimo + (maximo-minimo) * Math.random();
@@ -68,13 +71,17 @@ public class Main {
             StdDraw.clear();
 
             //Setear color, dibujar linea y mostrar linea
-            StdDraw.setPenColor(colores[(int) (Math.random()*colores.length)]);
-
+            StdDraw.setPenColor(colores[0]);
             StdDraw.line(x0,y0,x1,y1);
+            StdDraw.setPenColor(colores[1]);
             StdDraw.line(x0,y0+0.015,x1,y1+0.015);
+            StdDraw.setPenColor(colores[2]);
             StdDraw.line(x0,y0+0.030,x1,y1+0.030);
+            StdDraw.setPenColor(colores[3]);
             StdDraw.line(x0,y0+0.045,x1,y1+0.045);
+            StdDraw.setPenColor(colores[4]);
             StdDraw.line(x0,y0+0.060,x1,y1+0.060);
+            StdDraw.setPenColor(colores[5]);
             StdDraw.line(x0,y0+0.075,x1,y1+0.075);
 
             StdDraw.show();
